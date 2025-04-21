1a5aedbdc643dea88e5e5ebe92a80005ca084283
Eugene <yevheniisukhominskyi@gmail.com>
2025-04-21 14:53:29 +0300

    Created a models

---
+++ src/main/java/com/openbook/openbook/models/Book.java
@@ -0,0 +1,36 @@
+package com.openbook.openbook.models;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private Long characters;
+    private Integer price;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id", nullable = false)
+    private Genre genre;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    @JsonManagedReference
+    private Member author;
+}
---
+++ src/main/java/com/openbook/openbook/models/Member.java
@@ -0,0 +1,43 @@
+package com.openbook.openbook.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.openbook.openbook.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "author")
+    @JsonBackReference
+    private List<Book> books;
+
+    @ManyToMany
+    @JoinTable(
+            name = "library",
+            joinColumns = @JoinColumn(name = "reader_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private List<Book> library;
+}
