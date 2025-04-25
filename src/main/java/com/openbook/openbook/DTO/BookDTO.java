03a7cd1c4ff16b15642b2de8baf8990071dbed83
Eugene <yevheniisukhominskyi@gmail.com>
2025-04-25 22:57:32 +0300

    Edited bookDTO

--- src/main/java/com/openbook/openbook/DTO/BookDTO.java
+++ src/main/java/com/openbook/openbook/DTO/BookDTO.java
@@ -1,5 +1,7 @@
 package com.openbook.openbook.DTO;
 
+import com.openbook.openbook.enums.BookStatus;
+import com.openbook.openbook.services.BookService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,7 @@ public class BookDTO {
     private String description;
     private Long characters;
     private Integer price;
+    private BookStatus status;
     private Long genreId;
     private Long authorId;
 }
