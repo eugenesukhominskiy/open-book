b4c2e0696b09c05c347d12ce32bcd81d58de0417
Eugene <yevheniisukhominskyi@gmail.com>
2025-05-20 01:34:42 +0300

    Edited SwaggerConfig

--- src/main/java/com/openbook/openbook/config/SwaggerConfig.java
+++ src/main/java/com/openbook/openbook/config/SwaggerConfig.java
@@ -3,6 +3,9 @@ package com.openbook.openbook.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,12 +15,18 @@ import java.util.List;
 public class SwaggerConfig {
     @Bean
     public OpenAPI api() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
-                .servers(
-                        List.of(new Server().url("http://localhost:8080"))
-                )
-                .info(
-                        new Info().title("API documentation")
-                );
+                .servers(List.of(new Server().url("http://localhost:8080")))
+                .info(new Info().title("API documentation"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ));
     }
 }
